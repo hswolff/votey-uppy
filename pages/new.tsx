8ed4a26c7541932143e2619a6a92e042d61f13c0
Harry Wolff <hswolff@gmail.com>
2020-09-20 21:49:31 -0400

    Small tweak to new page

--- pages/new.tsx
+++ pages/new.tsx
@@ -1,11 +1,21 @@
 import ManageItemForm from 'components/ManageItemForm';
 import Card, { Header } from 'components/Card';
+import { signin, useSession } from 'next-auth/client';
 
 export default function NewItem() {
+  const [session] = useSession();
   return (
     <Card className="flex flex-col">
       <Header>Add New Item</Header>
-      <ManageItemForm />
+      {session && <ManageItemForm />}
+      {!session && (
+        <button
+          className="hover:text-purple-500"
+          onClick={() => signin('github')}
+        >
+          sign in to add a new item
+        </button>
+      )}
     </Card>
   );
 }
